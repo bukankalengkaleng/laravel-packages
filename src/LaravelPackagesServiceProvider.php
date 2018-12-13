41b361725f3809e629207462ec74c0c5b84d701b
R. Kukuh <rkukuh@gmail.com>
2018-12-13 07:18:13 +0700

    Create service provider

---
+++ src/LaravelPackagesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BukanKalengKaleng\LaravelPackages;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPackagesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
