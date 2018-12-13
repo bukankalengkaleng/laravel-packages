0abd6e131cfcf964be93cd5f70cb8c115e44054b
R. Kukuh <rkukuh@gmail.com>
2018-12-13 07:28:38 +0700

    Load route

--- src/LaravelPackagesServiceProvider.php
+++ src/LaravelPackagesServiceProvider.php
@@ -13,7 +13,7 @@ class LaravelPackagesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 
     /**
