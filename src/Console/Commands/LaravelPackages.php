3ca2d5877bdfbb44c720ce2b36a9be182f03b8dd
R. Kukuh <rkukuh@gmail.com>
2018-12-22 18:44:58 +0700

    Update information section

--- src/Console/Commands/LaravelPackages.php
+++ src/Console/Commands/LaravelPackages.php
@@ -168,6 +168,7 @@ class LaravelPackages extends Command
         $this->line('2. You may start Websockets server using: "php artisan websockets:serve"');
         $this->line('3. You may create Laravel Passport\'s client using: "php artisan passport:client"');
         $this->line('4. A premium license is required to be able to use DataTables Editor library');
+        $this->line('5. Laravel Socialite providers: https://socialiteproviders.github.io/providers/digital-ocean.html');
     }
 
     /**
