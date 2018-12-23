a24def8891c320516ed831f5eb9d001975f95de3
R. Kukuh <rkukuh@gmail.com>
2018-12-23 16:21:14 +0700

    Add unit test

---
+++ src/tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BukanKalengKaleng\LaravelPackages\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
---
+++ src/tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BukanKalengKaleng\LaravelPackages\Tests\Unit;
+
+use BukanKalengKaleng\LaravelPackages\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
